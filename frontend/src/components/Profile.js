7e258bac78fb0ee5cc076d6b829e8536b95918c0
kimad-cy <khadija.imad@um6p.ma>
2025-11-22 01:33:25 +0100

    Update Profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Box, Typography, Button, CircularProgress, Snackbar, Alert, Link } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { getAllEmployees } from '../services/employeeService';
